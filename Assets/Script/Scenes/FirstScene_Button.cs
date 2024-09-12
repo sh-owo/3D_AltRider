ec7fd6b63416057da9c4a9d5e813922d2b3db76c
sh-owo <sngho0718@gmail.com>
2024-09-13 08:58:53 +0900

    save

--- Assets/Script/Scenes/FirstScene_Button.cs
+++ Assets/Script/Scenes/FirstScene_Button.cs
@@ -6,6 +6,6 @@ public class ChangeSceneButton : MonoBehaviour
     // 씬 전환을 위한 메서드
     public void OnButtonClick(int sceneBuildIndex)
     {
-        SceneManager.LoadScene(sceneBuildIndex);
+        SceneManager.LoadScene("map");
     }
 }
